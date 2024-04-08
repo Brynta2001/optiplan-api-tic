87c597f5607c124c1bf26972c72b95574a22046c
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2024-04-08 18:44:45 -0500

    style: format main document with prettier

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes( 
-    new ValidationPipe({ 
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
-    }) 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   app.enableCors();
