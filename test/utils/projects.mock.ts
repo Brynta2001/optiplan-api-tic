4180e6a3e458065d94171ad581bc7e60a51cc525
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2024-04-10 15:34:49 -0500

    chore: add project and task mocks

---
+++ test/utils/projects.mock.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker';
+import { Project } from 'src/projects/entities/project.entity';
+import { mockAccounts } from './accounts.mock';
+
+export const mockProjects: Project[] = [
+  {
+    id: faker.string.uuid(),
+    title: 'Project 1',
+    description: 'Project 1 description',
+    code: faker.hacker.abbreviation(),
+    createdBy: mockAccounts[0],
+    states: [],
+    tasks: [],
+  },
+];
--- test/utils/tasks.mock.ts
+++ test/utils/tasks.mock.ts
@@ -3,36 +3,28 @@ import { CreateTaskDto } from 'src/tasks/dto/create-task.dto';
 import { UpdateTaskDto } from 'src/tasks/dto/update-task.dto';
 import { Task } from 'src/tasks/entities/task.entity';
 
-export const mockTaskRepository = {
-  create: jest.fn(),
-  save: jest.fn(),
-  find: jest.fn(),
-  findOneBy: jest.fn(),
-  preload: jest.fn(),
-  delete: jest.fn(),
-};
+export const mockTaskRepository = {};
 
-export const mockTasks = [
+export const mockTasks: Task[] = [
   {
     id: faker.string.uuid(),
     title: 'Recopilar requisitos',
     description: 'Agendar reunión con el cliente para recopilar requisitos',
-  } as Task,
-  {
-    id: 'a2d8d1c0-4b0b-4e6b-8e7b-5b1a1b7b1b1a',
-    title: 'Refinar las historias de usuario',
-    description: 'Refinar las historias de usuario con el equipo de desarrollo',
-  } as Task,
+    project: null,
+    state: null,
+    assignedTo: null,
+    createdBy: null,
+  },
 ];
 
-export const mockCreateTaskDto = {
+export const mockCreateTaskDto: CreateTaskDto = {
   title: 'Recopilar requisitos',
   description: 'Agendar reunión con el cliente para recopilar requisitos',
   stageId: faker.string.uuid(),
   projectId: faker.string.uuid(),
 } as CreateTaskDto;
 
-export const mockUpdateTaskDto = {
+export const mockUpdateTaskDto: UpdateTaskDto = {
   title: 'Recopilar requisitos de usuario',
   description:
     'Agendar la reunión con las partes interesadas para recopilar requisitos de usuario',
