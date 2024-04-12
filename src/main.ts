ca1c9dfdc8a6e9680cd3655dc6cce5148a852df2
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2024-04-12 00:51:14 -0500

    chore: add config for swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,18 @@ async function bootstrap() {
     origin: '*',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Optiplan API')
+    .setDescription(
+      'This is the documentation of the Optiplan API, an API for enterprise task management application with Kanban.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
